54dee43244aa456b768e7e5cf8ff48cce8f5b166
karosuu <54604110+karosuu@users.noreply.github.com>
2022-04-27 17:02:26 -0600

    Course in progres
    Token problem solved

--- cypress/integration/ticketbox.spec.js
+++ cypress/integration/ticketbox.spec.js
@@ -1,9 +1,24 @@
-describe('Integration test with visual testing', function() {
-    it('Loads the homepage', function() {
-      // Load the page or perform any other interactions with the app.
-      cy.visit('index.html');
+describe('Integration test with visual testing', function () {
+  beforeEach(() => cy.visit('index.html') );
+
+  it('Loads the homepage', function () {
+    // Load the page or perform any other interactions with the app.
+    cy.visit('index.html')
+
+    // Take a snapshot for visual diffing
+    cy.percySnapshot();
+  })
+
+  it('Checks for invalid email', () => {
+    cy.get('#email').type('johndoe-example.com');
+    cy.percySnapshot();
+  })
   
-      // Take a snapshot for visual diffing
-      cy.percySnapshot();
-    });
-  });
+  it('Enable submition after all mandatory fields are filled', () => { 
+    cy.get('#first-name').type('John')
+    cy.get('#last-name').type('Doe')
+    cy.get('#email').type('johndoe@example.com')
+    cy.get('#agree').check()
+    cy.percySnapshot();
+  })
+});
